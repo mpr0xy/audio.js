1751865099e82bc815f429846f099f650384d96d
mpr0xy <mpr0xy@live.com>
2014-09-09 17:21:22 +0800

    fix ; after }

--- markbook.js
+++ markbook.js
@@ -11,19 +11,19 @@ javascript:(function(){
     window.audioJs.run();
   }
   else{
-    if(!window.jQuery) {
-      try{
+    if (!window.jQuery) {
+      try {
         var scr = document.createElement('script');
-        scr.type='text/javascript';
-        scr.src='//lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js';
+        scr.type = 'text/javascript';
+        scr.src = '//lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js';
         scr.onload = script.onreadystatechange = function(){
-          if(!this.readyState || this.readyState=='loaded' || this.readyState=='complete'){
+          if (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete'){
             loadAudioJs();
           }
-        }
+        };
         document.getElementsByTagName('head')[0].appendChild(scr);
       }
-      catch(e){
+      catch (e){
         alert(e);
       }
     }
@@ -33,14 +33,14 @@ javascript:(function(){
   }
 
   function loadAudioJs(){
-   try{
-      var x=document.createElement('SCRIPT');
-      x.type='text/javascript';
-      x.src='//raw.githubusercontent.com/mpr0xy/audio.js/master/audio.js';
-      x.charset='utf-8';
+   try {
+      var x = document.createElement('SCRIPT');
+      x.type = 'text/javascript';
+      x.src = '//raw.githubusercontent.com/mpr0xy/audio.js/master/audio.js';
+      x.charset = 'utf-8';
       document.getElementsByTagName('body')[0].appendChild(x);
     }
-    catch(e){
+    catch (e){
       alert(e);
     } 
   }
